9f5b8fbb7d997da15433591df284d68078b41174
alexdum <alexandru.dumitrescu@gmail.com>
2015-10-01 11:07:11 +0300

    actualizare nume fisier statii

--- data-raw/stations.R
+++ data-raw/stations.R
@@ -1,6 +1,6 @@
-stations<-read.csv("data-raw/statii_meteo.csv")
-stations$X.1<-NULL
-stations$Y.1<-NULL
+weather_stations<-read.csv("data-raw/statii_meteo.csv")
+weather_stations$X.1<-NULL
+weather_stations$Y.1<-NULL
 
-devtools::use_data(stations, overwrite = TRUE)
+devtools::use_data(weather_stations, overwrite = TRUE)
 

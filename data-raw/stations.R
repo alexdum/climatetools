9ac6ca37bfdf0697dd1eddfed5ba3cd6df94eb90
alexdum <alexandru.dumitrescu@gmail.com>
2015-10-01 11:57:46 +0300

    actualizare R script

--- data-raw/stations.R
+++ data-raw/stations.R
@@ -1,6 +1,8 @@
-ws<-read.csv("data-raw/statii_meteo.csv")
-ws$X.1<-NULL
-ws$Y.1<-NULL
+library(rgdal)
+ws<-data.frame(readOGR(dsn="data-raw",layer="statii_aprilie_2008"))
+ws$coords.x1<-NULL
+ws$coords.x2<-NULL
+ws$optional<-NULL
 
 devtools::use_data(ws, overwrite = TRUE)
 

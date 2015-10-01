7e7f51406023da059f907ca0eab454c982c0456c
alexdum <alexandru.dumitrescu@gmail.com>
2015-10-01 11:10:51 +0300

    actualizare script date

--- data-raw/stations.R
+++ data-raw/stations.R
@@ -1,6 +1,6 @@
-weather_stations<-read.csv("data-raw/statii_meteo.csv")
-weather_stations$X.1<-NULL
-weather_stations$Y.1<-NULL
+ws<-read.csv("data-raw/statii_meteo.csv")
+ws$X.1<-NULL
+ws$Y.1<-NULL
 
-devtools::use_data(weather_stations, overwrite = TRUE)
+devtools::use_data(ws, overwrite = TRUE)
 

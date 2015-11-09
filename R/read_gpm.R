70fa5c550b79ed5b04ee92d09cb8dbc701e7090e
alexdum <alexandru.dumitrescu@gmail.com>
2015-11-09 20:52:00 +0200

    actualizare help read_gmp

--- R/read_gpm.R
+++ R/read_gpm.R
@@ -1,7 +1,7 @@
 #' Read GMP data to raster file
 #' @param file cclea catre fisierul h5
 #' @param dataset tipul de dataset care se doreste a se citi
-#' @param extent of the gridfile to be read in R
+#' @param extent of the gridfile to be read in R, a vector of a length 4; order= xmin, xmax, ymin, ymax)
 #' @details
 #' Fisierele sunt desarcate de aici ftp://jsimpson.pps.eosdis.nasa.gov/data/imerg/early/
 #' @return raster object with layer names from dataset
@@ -13,7 +13,7 @@
 #' plot(r)
 #' @export
 
-read_gpm<-function (file,dataset,extent)
+read_gpm<-function (file,dataset,extent=NA)
 {
 
 
@@ -33,13 +33,17 @@ read_gpm<-function (file,dataset,extent)
   ymax<-which.min(abs(lat[]-extent[4]))
 
   #raster definition
+  if (is.na(extent))
+  {
+    r<-raster::raster(x=apply(pp[], 1, rev), xmn=-180, xmx=180, ymn=-90, ymx=90,crs="+init=epsg:4326")
 
-  r<-raster::raster(x=apply(pp[][xmin:xmax,ymin:ymax], 1, rev), xmn=lon[][which.min(abs(lon[]-extent[1]))]-0.05,
-                    xmx=lon[][which.min(abs(lon[]-extent[2]))]+0.05, ymn=lat[][which.min(abs(lat[]-extent[3]))]-0.05,
-                    ymx=lat[][which.min(abs(lat[]-extent[4]))]+0.05,crs="+init=epsg:4326")
+  }
+  else {
+    r<-raster::raster(x=apply(pp[][xmin:xmax,ymin:ymax], 1, rev), xmn=lon[][which.min(abs(lon[]-extent[1]))]-0.05,
+                      xmx=lon[][which.min(abs(lon[]-extent[2]))]+0.05, ymn=lat[][which.min(abs(lat[]-extent[3]))]-0.05,
+                      ymx=lat[][which.min(abs(lat[]-extent[4]))]+0.05,crs="+init=epsg:4326")
 
-  r<-raster::raster(x=apply(pp[], 1, rev), xmn=-180, xmx=180, ymn=-90, ymx=90,crs="+init=epsg:4326")
-
+  }
 
   r<- raster::calc(r, fun.na)
   #adauga nume fisier

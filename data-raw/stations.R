015be6d6962d95e230e3da865ca5d5f6fdd2efe2
alexdum <alexandru.dumitrescu@gmail.com>
2016-02-24 17:33:46 +0200

    adaugare statia faurei

--- data-raw/stations.R
+++ data-raw/stations.R
@@ -4,5 +4,17 @@ ws$coords.x1<-NULL
 ws$coords.x2<-NULL
 ws$optional<-NULL
 
+# completeaza statia Faurei
+ws[164,"CODGE"] <- 505719
+ws[164,"JU"] <- "BR"
+ws[164,"X"] <- 679848.2
+ws[164,"Y"] <- 400729.2
+ws[164,"Z"] <- 41
+ws[164, "Lon"] <- 27.28333333
+ws[164, "Lat"] <- 45.08333333
+ws[164,"CMR"] <- "MUNTENIA"
+
+
 devtools::use_data(ws, overwrite = TRUE)
+
 

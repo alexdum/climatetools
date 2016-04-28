a003534fbb18daa603ccf90aa245498c03a4f83d
alexdum <alexandru.dumitrescu@gmail.com>
2016-04-28 16:08:39 +0300

    adaugare posturi

--- data-raw/stations.R
+++ data-raw/stations.R
@@ -24,14 +24,20 @@ ex <- round(extract(rug, sp),3)
 ws$Rugozitate <- ex
 
 
+# adauga posturi ----------------------------------------------------------
+
+post <- read.csv("data-raw/posturi_co_2016 .csv")
+
+ws <- rbind(ws, post)
+
+
 # adauga Cau --------------------------------------------------------------
 cau <- read.csv("data-raw/cau.csv")
 names(cau)[5:7] <- c("Cau_0_20","Cau_0_50","Cau_0_100")
 
 ws <- merge(ws,cau[,c("co","Cau_0_20","Cau_0_50","Cau_0_100")], by.x = "CODST", by.y = "co", all.x = T)
 
-
-
 devtools::use_data(ws, overwrite = TRUE)
+
 
 

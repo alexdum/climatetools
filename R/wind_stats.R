b76d4a62d4f66bd782b323deed12c9848640e302
alexdum <alexandru.dumitrescu@gmail.com>
2016-05-04 14:32:51 +0300

    update wind stats

--- R/wind_stats.R
+++ R/wind_stats.R
@@ -1,7 +1,15 @@
 #'Weibull parameters and wind speed characteristics
 #'@param x a numeric vector containing hourly wind speed data
-#'@param rho air density. Default value is 1.225 $kgm^−3$ for a
-#'temperature of 15◦C and a standard sea level pressure of 1013.25 hPa
+#'@param rho air density. Default value is 1.225 kgm^−3 for a
+#'temperature of 15°C and a standard sea level pressure of 1013.25 hPa
+#'@return A list with four components:
+#'\itemize{
+#'\item mean value:
+#'\item max. power wind speed:
+#'\item wind power density:
+#'\item A scale parameter
+#'\item k shape parameter
+#'}
 #'@details Calculate some simple characteristics
 #'of a Weibull distribution function, given the scale parameter A and the
 #'shape parameter k. The Weibull characteristics calculated are (Troen and Petersen, 1989):
@@ -10,6 +18,7 @@
 #'\item max. power wind speed:
 #'\item wind power density:
 #'}
+#'@export
 
 
 wind_stats <- function(x, rho = 1.225) {

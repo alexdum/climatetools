87d080c6dc4cb48b256b989cbb533b26fa5760b6
alexdum <alexandru.dumitrescu@gmail.com>
2016-06-10 13:11:49 +0300

    ass weather data

--- data-raw/weather_data.R
+++ data-raw/weather_data.R
@@ -1,6 +1,6 @@
 buc_baneasa <- read.csv("data-raw/bucharest_baneasa.csv")
 buc_baneasa$Date <- as.Date(buc_baneasa$Date)
-
+names(buc_baneasa)[1] <- "CODGE"
 devtools::use_data(buc_baneasa, overwrite = TRUE)
 
 

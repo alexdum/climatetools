81a3bf1062dcfe53d9640802a0594c7755d300ed
alexdum <alexandru.dumitrescu@gmail.com>
2017-05-24 12:03:21 +0300

    actualizare functie calcul et0

--- R/et_0.R
+++ R/et_0.R
@@ -6,8 +6,11 @@
 #'@param Lon longitude in degrees (WGS84; EPSG:4326)
 #'@param Lat latitude in degrees (WGS84; EPSG:4326)
 #'@param Dates date field reprezenting days of measurements
-#'@param Tavg daily mean temperature (ºC)
+#'@param Tmax daily maximum temperature (ºC)
+#'@param Tmin daily minimum temperature (ºC)
 #'@param Rh daily mean relative humidity (\%)
+#'@param RHmax daily maximum relative humidity (\%)
+#'@param RHmin daily minimum relative humidity (\%)
 #'@param Sd daily sunshine duration (hours)
 #'@param Rs daily sunshine duration (MJ m-2 day-1)
 #'@param Ws daily wind speed (m/s) at 2m above ground level
@@ -26,52 +29,45 @@
 #'@importFrom  maptools sunriset
 #'@export
 
-et_0 <- function(Alt, Lon, Lat, Dates, Tmax,Tmin, RHmax, RHmin, Sd = NA, Rs = NA, Ws) {
+et_0 <- function(Alt, Lat, Dates, Tmax, Tmin, Rh = NA, RHmax, RHmin, Sd = NA, Rs = NA, Ws) {
 
   # dd <- read.csv("data-raw/bucharest_baneasa.csv")
-  # Lon = 26.07969
-  # Lat = 44.51072
+  # Lon = 4.3517
+  # Lat = 50.80
   # Alt <- 100
   # Tavg <- 16.9
   # Tmax <- 21.5
   # Tmin <- 12.3
+  # Sd = 9.25
   # Rs <- 22.07
   # RHmax <- 84
   # RHmin <- 63
   # Ws <- 2.078
   # Dates <- as.Date("2015-07-06")
-  # Lat <- 50.80
   # convert coordinate to Spatial Points
-  hels <- matrix(c(Lon, Lat), nrow = 1)
-  Hels <- SpatialPoints(hels, proj4string = CRS("+proj=longlat +datum=WGS84"))
 
-
-  # compute dy length
-  up <- sunriset(Hels, as.POSIXct(Dates, tz = "EET"), direction = "sunrise", POSIXct.out = TRUE)
-  down <- sunriset(Hels, as.POSIXct(Dates, tz = "EET"), direction = "sunset", POSIXct.out = TRUE)
-  dl <- as.numeric(down$time - up$time)
-
-
-  # extract Julian dai and latitude
+  # extract Julian day and latitude
   J <- as.integer(format(Dates,"%j"))
   lat <- Lat
 
   tt <- Tavg
-  ur <- Rh
+  if (!is.na(Rh)) rh <- Rh
   if (!is.na(Sd)) ds <- Sd
+  if (!is.na(Rs)) rs <- Rs
   ws <- Ws
   Alt <- Alt
   tmax <- Tmax
   tmin <- Tmin
   rhmax <- RHmax
   rhmin <- RHmin
-  rs <- Rs
+
+
 
   # Atmospheric Pressure (P) kPa
   P <- 101.3*(((293 - 0.0065*Alt)/293))^5.26
 
   # Slope of saturation vapor pressure curve (Δ) kPa/°C
-  D <- 4098*(0.6108*exp(17.27*tt/(tt+237.3)))/(tt + 237.3)^2
+  D <- 4098*(0.6108*exp(17.27*tt/(tt + 237.3)))/(tt + 237.3)^2
 
   # Psychrometric constant (γ)  kPa °C-1
   g <- 0.000665*P
@@ -94,7 +90,7 @@ et_0 <- function(Alt, Lon, Lat, Dates, Tmax,Tmin, RHmax, RHmin, Sd = NA, Rs = NA
   ea <- (esmin*(RHmax/100) + esmax*(RHmin/100))/2
 
   # Vapour pressure deficit
-  ed <- es-ea
+  ed <- es - ea
 
   # The inverse relative distance Earth-Sun (dr) and solar declination (d)
   dr <- 1 + 0.033*cos(2*pi*J/365)
@@ -109,14 +105,21 @@ et_0 <- function(Alt, Lon, Lat, Dates, Tmax,Tmin, RHmax, RHmin, Sd = NA, Rs = NA
   # Step 15: Extraterrestrial radiation (Ra)
   ra <- (24*60)/pi*0.0820*dr*((wsh*sin(lat.rad)*sin(d)) + (cos(lat.rad)*cos(d)*sin(wsh)))
 
-  # Step 16: Clear sky solar radiation (Rso)
+   # Step 16: Clear sky solar radiation (Rso)
   rs0 <- (0.75 + 0.00002*Alt)*ra
+
+  # sloar radiation from sunshine duration
+  # daylight hours (N)
+  if (is.na(Sd[1])) {
+  n <- 24/pi*wsh
+  rs <- (0.25 + 0.50*(ds/n))*ra
+  }
 
   # Step 17: Net solar or net shortwave radiation (Rns)
   rns <- (1 - 0.23)*rs
 
   # Step 18: Net outgoing long wave solar radiation (Rnl)
-  rnl <- 0.000000004903*((tmax+273.16)^4 + (tmin+273.16)^4)/2*(0.34-0.14*sqrt(ea))*(1.35*(rs/rs0)-0.35)
+  rnl <- 0.000000004903*((tmax + 273.16)^4 + (tmin + 273.16)^4)/2*(0.34 - 0.14*sqrt(ea))*(1.35*(rs/rs0) - 0.35)
 
   # Step 19: Net radiation (Rn)
   rn <- rns - rnl
@@ -130,53 +133,8 @@ et_0 <- function(Alt, Lon, Lat, Dates, Tmax,Tmin, RHmax, RHmin, Sd = NA, Rs = NA
   # FS2. Wind term (ETwind)
   ETwind = Pt * Tt * (es - ea)
 
-  ETo <-  round(ETwind + ETrad,1)
-
-  # saturated vapour presure
-  pm <- 6.11*10^((7.5*t)/(237.7 + t))
-  phg <- pm*0.0295300
-
-  # kpa saturated vapour presure
-  ea <- 33.8639*(phg/10)
-
-  #actual vapour presure
-  ed <- ur*ea/100
-  ead <- ea - ed
-
-  #pas 2 Calculate available energy
-  alpha<-(3.14/180)*(lat)
-  dr <- 1 + 0.033*cos(2*3.14*(J)/365)
-  beta <- 0.409*sin(2*3.14*(J)/365 - 1.39)
-  omegas<-acos(-tan(alpha)*tan(beta))
-  var1<-sin(alpha)*sin(beta)
-  var2<-cos(alpha)*cos(beta)
-  # extraterestrial radiation
-  Ra <- 24*60/3.14*0.0820*dr*(omegas*(var1) + var2*sin(omegas))
-
-  Rnl = 2.45*10^-9*(0.9*ds/dl + 0.1)*(0.34 - 0.14*sqrt(ed))*2*(273 + t)^4
-
-
-  # calculeaza in functie daca ai radiatie globala sau stralucire Soare
-  if (!is.na(Rs) & is.na(Sd)) {
-    RnG = Rs - Rnl - 0
-  } else {
-    Rns = (0.25 + 0.5*ds/dl)*Ra
-    # Rns = 0.77*(0.25 + 0.5*ds/dl)*Ra
-    RnG = Rns - Rnl - 0
-  }
-
-
-
-
-  # pas 3 calculate other values required
-
-  # slope of saturated vapour pessure curve
-  slope <- 4098*ea/(t+237.3)^2
-
-  # pas 4 final
-
-  et<-(0.408*slope*RnG)/(slope+0.066*(1+0.34*ws))+(0.066*(900/(t+273)*ws*ead))/(slope+0.066*(1+0.34*ws))
-  return(et)
+  ET0 <-  round(ETwind + ETrad,1)
+  return(ET0)
 }
 
 

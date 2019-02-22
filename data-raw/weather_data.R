ccbbd9bf58b3cc2a2aba6919ea2131b0f35c0403
Alexandru Dumitrescu <alexandrudumitrescu@MacBook-Pro.local>
2019-02-22 20:50:26 +0200

    usethis

--- data-raw/weather_data.R
+++ data-raw/weather_data.R
@@ -1,7 +1,7 @@
 buc_baneasa <- read.csv("data-raw/bucharest_baneasa.csv")
 buc_baneasa$Date <- as.Date(buc_baneasa$Date)
 names(buc_baneasa)[1] <- "CODGE"
-devtools::use_data(buc_baneasa, overwrite = TRUE)
+usethis::use_data(buc_baneasa, overwrite = TRUE)
 
 
 

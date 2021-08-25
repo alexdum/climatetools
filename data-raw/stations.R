93e51b73614f652ba0c28a909404f8561a3cf953
alexdum <alexandru.dumitrescu@gmail.com>
2021-08-25 16:07:17 +0300

    adaugare fundulea

--- data-raw/stations.R
+++ data-raw/stations.R
@@ -8,28 +8,41 @@ ws <- ws[, !names(ws) %in% c("coords.x1", "coords.x2", "optional")]
 names(ws)[c(8:11)] <- c("Lon", "Z", "X","Y")
 
 
-# completeaza statia Faurei
-ws[164,"CODGE"] <- 505719
-ws[164,"JU"] <- "BR"
-ws[164,"X"] <- 679848.2
-ws[164,"Y"] <- 400729.2
-ws[164,"Z"] <- 41
-ws[164, "Lon"] <- 27.28333333
-ws[164, "Lat"] <- 45.08333333
+# completeaza statia Fundulea
+ws[164,"CODST"] <- 15424
+ws[164,"CODGE"] <- 428632
+ws[164,"JU"] <- "CL"
+ws[164,"X"] <- 621373
+ws[164,"Y"] <- 329264
+ws[164,"Z"] <- 67
+ws[164, "Lon"] <- 26.523548
+ws[164, "Lat"] <- 44.452850
 ws[164,"CMR"] <- "MUNTENIA"
+ws[166,"NUME"] <- "Fundulea"
+
+# completeaza statia Faurei
+ws[165,"CODGE"] <- 505719
+ws[165,"JU"] <- "BR"
+ws[165,"X"] <- 679848.2
+ws[165,"Y"] <- 400729.2
+ws[165,"Z"] <- 41
+ws[165, "Lon"] <- 27.28333333
+ws[165, "Lat"] <- 45.08333333
+ws[165,"CMR"] <- "MUNTENIA"
+ws[166,"NUME"] <- "Faurei"
 
 # completeaza statie Sinaia Mănăstire
-ws[165,"CODGE"] <- 521534
-ws[165,"JU"] <- "PH"
-ws[165,"X"] <- 543110.5
-ws[165,"Y"] <- 428607.9
-ws[165,"Z"] <- 560
-ws[165, "Lon"] <- 25.548755
-ws[165, "Lat"] <- 45.355916
-ws[165,"CMR"] <- "MUNTENIA"
-ws[165,"NUME"] <- "Sinaia Mănăstire"
+ws[166,"CODGE"] <- 521534
+ws[166,"JU"] <- "PH"
+ws[166,"X"] <- 543110.5
+ws[166,"Y"] <- 428607.9
+ws[166,"Z"] <- 560
+ws[166, "Lon"] <- 25.548755
+ws[166, "Lat"] <- 45.355916
+ws[166,"CMR"] <- "MUNTENIA"
+ws[166,"NUME"] <- "Sinaia Mănăstire"
 
-
+ws <- ws[order(ws$NUME),]
 
 
 
